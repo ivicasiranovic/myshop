f75dbfcd953206e962d99500c747a2f50967acbd
Ivica Siranovic <ivicasiranovic@gmail.com>
2023-08-29 17:31:52 +0200

    Custom Hook

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,10 +1,20 @@
 import React from "react";
+import useLocalStorage from "./useLocalStorage"; // Adjust the path as needed
 
 const Header = () => {
+  const [isDarkMode, setIsDarkMode] = useLocalStorage("darkMode", false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div className="ui fixed menu">
+    <div className={`ui fixed menu ${isDarkMode ? "inverted" : ""}`}>
       <div className="ui container center">
         <h2>MyShop</h2>
+        <button onClick={toggleDarkMode}>
+          Toggle Dark Mode: {isDarkMode ? "On" : "Off"}
+        </button>
       </div>
     </div>
   );
