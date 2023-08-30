d8b4a5f1911930e3b47261c0fc8d3933f0b50515
Ivica Siranovic <ivicasiranovic@gmail.com>
2023-08-30 16:12:29 +0200

    Deploy2

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <Router>
         <App />
-      </HashRouter>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
